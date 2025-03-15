e31608447316aa6318a28b0255d4903935cdf985
Ziwen (Simon) Su <ziwen.su@apod.com.au>
2025-03-15 14:26:38 +1030

    typescript env setup, mock up rating widget

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RatingWidget from './component/Rating';
+
+function App() {
+  return (
+    <div className="App">
+      <RatingWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Rating/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function RatingWidget() {
+    return (
+        <div>
+            <p>You feedback is valuable</p>
+            <p>How would you rate your experience?*</p>
+            <div>5 stars</div>
+            <p>What was the main purpose of your visit?</p>
+            <input></input>
+            <p>Is there anything we can improve?</p>
+            <input></input>
+        </div>
+    )
+}
