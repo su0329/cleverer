3fcc35147886bd3fd79a2761ccf6562ba1a44c67
Ziwen (Simon) Su <ziwen.su@apod.com.au>
2025-03-15 23:41:02 +1030

    enable list pagination, page navigation

--- src/component/List/UserRatingList/index.tsx
+++ src/component/List/UserRatingList/index.tsx
@@ -3,11 +3,10 @@ import { localStorageHelper } from "../../../Utility/localStorage";
 import { UserRating } from "../../Rating";
 import "./index.css";
 
-
-
 export function UserRatingList(){
-    const [pageIndex, setPageIndex] = React.useState(0);
-    const [pagination, setPagination] = React.useState<10 | 20 | 50>(10);
+    const [pageIndex, setPageIndex] = React.useState(1);
+    const [pageIndexInput, setPageIndexInput] = React.useState(pageIndex);
+    const [pagination, setPagination] = React.useState<number>(10);
     const [sortby, setSortBy] = React.useState<'starRating asc' | 'starRating desc' | 'id acs' | 'id desc'>('id acs')
     
     const paginatedData = React.useMemo(() => {
@@ -41,22 +40,59 @@ export function UserRatingList(){
         return paginatedData;
     }, [pagination, sortby]);
 
+    const goToPage = () => {
+        if (pageIndexInput <= 1){
+            setPageIndex(1);
+            setPageIndexInput(1);
+            return;
+        }
+
+        if (pageIndexInput > paginatedData.length){
+            setPageIndex(paginatedData.length);
+            setPageIndexInput(paginatedData.length);
+            return;
+        }
+
+        setPageIndex(pageIndexInput);
+        setPageIndexInput(pageIndexInput);
+    };
+
+    const incrementPageIndex = () => {
+        if (pageIndex === paginatedData.length){
+            return;
+        }
+
+
+        setPageIndex(index => ++index);
+        setPageIndexInput(index => ++index)
+    };
+
+    const decrementPageIndex = () => {
+        if (pageIndex === 1){
+            return;
+        }
+
+        setPageIndex(index => --index);
+        setPageIndexInput(index => --index);
+    };
+
     if (!!!paginatedData || paginatedData.length === 0) {
         return <div>No data to display</div>
     }
 
     const headers = ['ID', 'Rating', 'Purpose of Visiting', 'Improvement'];
 
-
     return (
         <div>
             <table>
                 <thead>
-                    {headers.map((header, i) => <th key={`header ${i}`}>{header}</th>)}
+                    <tr>
+                        {headers.map((header, i) => <th key={`header ${i}`}>{header}</th>)}
+                    </tr>
                 </thead>
                 <tbody>
                     {
-                    paginatedData[pageIndex].map((value, i) => (
+                    paginatedData[pageIndex-1].map((value, i) => (
                         <tr key={`user rating ${i}`}>
                             <td>{value.id}</td>
                             <td>{value.starRating}</td>
@@ -69,23 +105,23 @@ export function UserRatingList(){
             <div className="table-tool-container">
                 <div className="table-tool">
                     <label htmlFor="pagination-select">Rows per page: </label>
-                    <select id="pagination-select">
-                        <option>10</option>
-                        <option>20</option>
-                        <option>50</option>
+                    <select id="pagination-select" value={pagination} onChange={(event)=>setPagination(Number(event.target.value))}>
+                        <option value={10}>10</option>
+                        <option value={20}>20</option>
+                        <option value={50}>50</option>
                     </select>
                 </div>
                 
                 <div className="table-tool">
                     <label htmlFor="pageIndex-input">Jump to: </label>
-                    <input id="pageIndex-input" defaultValue={pageIndex} />
-                    <button>go</button>
+                    <input type="number" id="pageIndex-input" value={pageIndexInput} onChange={(event)=>setPageIndexInput(Number(event.target.value))}/>
+                    <button onClick={goToPage}>go</button>
                 </div>   
 
                 <div className="table-tool">
-                    <button>{`<`}</button>
-                    <br />
-                    <button>{`>`}</button>
+                    <button onClick={decrementPageIndex}>{`<`}</button>
+                    <hr />
+                    <button onClick={incrementPageIndex}>{`>`}</button>
                 </div>
             </div>
         </div>
